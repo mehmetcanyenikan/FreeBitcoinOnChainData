f61b798ff6f9e4541948292295c1072e5e677b6b
mehmetcanyenikan <mehmetcanyenikan@gmail.com>
2024-08-22 17:20:15 +0300

    Blockchain.com onchain data

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import pandas as pd
+from connection import DataReadCondition
 
 
 
@@ -19,3 +20,29 @@ class DictToDataFrame(ConvertData):
     def dict_to(self) -> pd.DataFrame:
         return pd.DataFrame(self.dictionary)
 
+
+
+class DataFetcher:
+    def __init__(self, urls):
+        self.urls = urls
+
+    def fetch_data(self):
+        df_list = []
+        for url_key, url_value in self.urls.items():
+            reader = DataReadCondition.create_reader(f'https://api.blockchain.info/charts/{url_value}?timespan=6years&rollingAverage=24hours&format=json')
+            df = DictToDataFrame(reader.read()['values']).dict_to()
+            df = df.set_index('x')
+            df.index = pd.to_datetime(df.index, unit='s').date
+
+            df.columns = [f"{url_key}_{col}" for col in df.columns]
+            df_list.append(df)
+        return df_list
+
+
+
+class DataCombiner:
+    def __init__(self, data_frames):
+        self.data_frames = data_frames
+
+    def combine_data(self):
+        return pd.concat(self.data_frames, axis=1)
--- main.py
+++ main.py
@@ -1,51 +1,49 @@
 import pandas as pd
 from connection import DataReadCondition
-from data_processing import  DictToDataFrame
-
-
-
-urls = {
-        'total_circulating_bitcoin' : 'total-bitcoins',
-        'market_price_usd' : 'market-price',
-        'market_capitalization' : 'market-cap',
-        'exchange_trade_volume' : 'trade-volume',
-        'blockchain_size' : 'blocks-size',
-        'average_block_size' : 'avg-block-size',
-        'average_transactions_per_block' : 'n-transactions-per-block',
-        'total_number_of_transactions' : 'n-transactions-total',
-        'median_confirmation_time' : 'median-confirmation-time',
-        'average_confirmation_time' : 'avg-confirmation-time',
-        'total_hash_rate' : 'hash-rate',
-        'network_difficulty' : 'difficulty',
-        'miners_revenue' : 'miners-revenue',
-        'total_transactions_fees_btc' : 'transaction-fees',
-        'total_transactions_fees_usd' : 'transaction-fees-usd',
-        'cost_%_of_transaction_volume' : 'cost-per-transaction-percent',
-        'cost_per_transaction' : 'cost-per-transaction',
-        'unique_addresses_used' : 'n-unique-addresses',
-        'confirmed_transactions_per_day' : 'n-transactions',
-        'transaction_rate_per_second' : 'transactions-per-second',
-        'output_value_per_day' : 'output-volume',
-        'mempool_transaction_count' : 'mempool-count',
-        'mempool_size_growth' : 'mempool-growth',
-        'mempool_size_bytes' : 'mempool-size',
-        'unspent_transaction_outputs' : 'utxo-count',
-        'transactions_excluding_popular_addresses' : 'n-transactions-excluding-popular',
-        'estimated_transaction_value_btc' : 'estimated-transaction-volume',
-        'estimated_transaction_value_usd' : 'estimated-transaction-volume-usd'
-        }
+from data_processing import  DictToDataFrame, DataCombiner, DataFetcher
+import datetime
 
 
 
 def main():
-    
-    for url in urls:
-        reader = DataReadCondition.create_reader('https://api.blockchain.info/charts/{}?timespan=4years&rollingAverage=12hours&format=json'.format(urls[url]))
-        df = DictToDataFrame(reader.read()['values']).dict_to()
-        df = df.set_index('x')
-        print(df)
+    urls = {
+#        'total_circulating_bitcoin' : 'total-bitcoins',
+        'market_price_usd' : 'market-price',    # The market price is how much you can sell 1 Bitcoin (BTC) for. 
+#        'market_capitalization' : 'market-cap',
+        'exchange_trade_volume' : 'trade-volume',   # The Bitcoin trading volume indicates how many Bitcoins are being bought and sold on specific exchanges.
+        'blockchain_size' : 'blocks-size',  # The total size of the blockchain minus database indexes in megabytes.
+        'average_block_size' : 'avg-block-size',    # The average block size over the past 24 hours in megabytes.
+        'average_transactions_per_block' : 'n-transactions-per-block',  # The average number of transactions per block over the past 24 hours.
+        'total_number_of_transactions' : 'n-transactions-total',    # The total number of transactions on the blockchain.
+        'median_confirmation_time' : 'median-confirmation-time',    # The median time for a transaction with miner fees to be included in a mined block and added to the public ledger.
+        'average_confirmation_time' : 'avg-confirmation-time',      # The average time for a transaction with miner fees to be included in a mined block and added to the public ledger.
+        'total_hash_rate' : 'hash-rate',    # The estimated number of terahashes per second the bitcoin network is performing in the last 24 hours.
+        'network_difficulty' : 'difficulty',    # A relative measure of how difficult it is to mine a new block for the blockchain.
+        'miners_revenue' : 'miners-revenue',    # Total value in USD of coinbase block rewards and transaction fees paid to miners.
+        'total_transactions_fees_btc' : 'transaction-fees', # The total BTC value of all transaction fees paid to miners. This does not include coinbase block rewards.
+        'total_transactions_fees_usd' : 'transaction-fees-usd', # The total USD value of all transaction fees paid to miners. This does not include coinbase block rewards.
+        'cost_%_of_transaction_volume' : 'cost-per-transaction-percent', # A chart showing miners revenue as percentage of the transaction volume.
+        'cost_per_transaction' : 'cost-per-transaction',    # A chart showing miners revenue divided by the number of transactions.
+        'unique_addresses_used' : 'n-unique-addresses', # The total number of unique addresses used on the blockchain.
+        'confirmed_transactions_per_day' : 'n-transactions',    # The total number of confirmed transactions per day.
+#        'transaction_rate_per_second' : 'transactions-per-second',
+        'output_value_per_day' : 'output-volume',   # The total value of all transaction outputs per day. This includes coins returned to the sender as change.
+#        'mempool_transaction_count' : 'mempool-count',
+#        'mempool_size_growth' : 'mempool-growth',
+#        'mempool_size_bytes' : 'mempool-size',
+#        'unspent_transaction_outputs' : 'utxo-count',
+        'transactions_excluding_popular_addresses' : 'n-transactions-excluding-popular',    # The total number of transactions excluding those involving the network's 100 most popular addresses.
+        'estimated_transaction_value_btc' : 'estimated-transaction-volume', # The total estimated value in BTC of transactions on the blockchain. This does not include coins returned as change.
+        'estimated_transaction_value_usd' : 'estimated-transaction-volume-usd'  # The total estimated value in USD of transactions on the blockchain. This does not include coins returned as change.
+        }
 
+    fetcher = DataFetcher(urls)
+    df_list = fetcher.fetch_data()
 
+    combiner = DataCombiner(df_list)
+    combined_df = combiner.combine_data()
+
+    print(combined_df)
 
 if __name__ == '__main__':
     main()
